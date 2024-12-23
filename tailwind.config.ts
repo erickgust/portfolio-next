15110fa5e4b65949482e088dba91673cee048f79
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2024-12-23 11:34:53 -0300

    update tailwind configuration to add custom font weights and extend font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,18 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontWeight: {
+      normal: '450',
+    },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontSize: {
+        // 52px
+        "5.5xl": "3.25rem",
+      }
     },
   },
   plugins: [],
