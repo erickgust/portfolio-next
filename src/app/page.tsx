3b68098c5fef2440cc9ee661ff6d63b4e504ec48
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2024-12-25 03:31:32 -0300

    feat: add mycontacts project and their icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -144,7 +144,7 @@ export default function Home() {
               </div>
               <div />
               <div>
-                <div className="flex gap-4 justify-end">
+                <div className="flex gap-4 justify-end max-w-80 flex-wrap ml-auto">
                   <span
                     className="inline-flex rounded-full border py-2 px-4 gap-2.5 items-center justify-center leading-none text-sm text-[#8AD3C8] border-[#8AD3C8]"
                   >
@@ -181,6 +181,130 @@ export default function Home() {
                   Integer eget neque faucibus nulla eget sit diam. Volutpat phasellus nec venenatis massa turpis varius. <br />
                   Integer eget neque faucibus nulla eget sit diam. Volutpat phasellus nec venenatis massa turpis varius.Integer eget neque faucibus nulla eget sit diam. Volutpat phasellus nec venenatis massa turpis varius.
                 </p>
+              </div>
+            </div>
+          </article>
+          
+          <article>
+            <Image
+              src="/mycontacts-bg.jpg"
+              alt="MyContacts website screenshot"
+              width={1080}
+              height={408}
+              className="h-[25.5rem] w-full object-cover"
+            />
+
+            <div className="mt-14 grid grid-cols-[1fr_230px_1fr]">
+              <div>
+                <strong className="font-semibold text-3.5xl text-[#DCD7CD] leading-none">MyContacts</strong>
+
+                <p className="mt-4">
+                  Lorem ipsum dolor sit amet consectetur. Id parturient nisl accumsan euismod sit morbi pulvinar. Accumsan elit in dictum suspendisse varius.  <br />
+                  Integer eget neque faucibus nulla eget sit diam. Volutpat phasellus nec venenatis massa turpis varius.
+                </p>
+
+                <div className="flex gap-4 mt-8">
+                  <a 
+                    href="#"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="inline-flex rounded-full border py-2 px-4 gap-2.5 items-center justify-center leading-none text-sm text-[#E7BE6C] border-[#E7BE6C]"
+                  >
+                    <Image
+                      aria-hidden
+                      src="/arrow.svg"
+                      alt="Link icon"
+                      width={14}
+                      height={14}
+                    />
+                    <span className="pt-0.5">
+                      See live
+                    </span>
+                  </a>
+
+                  <a
+                    href="https://github.com/erickgust/my-contacts-front"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="inline-flex rounded-full border py-2 px-4 gap-2.5 items-center justify-center leading-none text-sm text-[#8E9AE1] border-[#8E9AE1]"
+                  >
+                    <Image
+                      aria-hidden
+                      src="/repository.svg"
+                      alt="Link icon"
+                      width={14}
+                      height={14}
+                    />
+
+                    <span className="pt-0.5">
+                      GitHub repository
+                    </span>
+                  </a>
+                </div>
+              </div>
+              <div />
+              <div>
+                <div className="flex gap-4 justify-end max-w-80 flex-wrap ml-auto">
+                  <span
+                    className="inline-flex rounded-full border py-2 px-4 gap-2.5 items-center justify-center leading-none text-sm text-[#88E6FF] border-[#88E6FF]"
+                  >
+                    <Image
+                      aria-hidden
+                      src="/techs/react.svg"
+                      alt="React icon"
+                      width={16}
+                      height={16}
+                    />
+                    <span className="pt-0.5">
+                      React.js
+                    </span>
+                  </span>
+
+                  <span
+                    className="inline-flex rounded-full border py-2 px-4 gap-2.5 items-center justify-center leading-none text-sm text-[#A6C6E0] border-[#A6C6E0]"
+                  >
+                    <Image
+                      aria-hidden
+                      src="/techs/postgresql.svg"
+                      alt="PostgreSQL icon"
+                      width={16}
+                      height={16}
+                    />
+                    <span className="pt-0.5">
+                      PostgreSQL
+                    </span>
+                  </span>
+                  
+                  <span
+                    className="inline-flex rounded-full border py-2 px-4 gap-2.5 items-center justify-center leading-none text-sm text-[#72B5FF] border-[#72B5FF]"
+                  >
+                    <Image
+                      aria-hidden
+                      src="/techs/typescript.svg"
+                      alt="TypeScript icon"
+                      width={16}
+                      height={16}
+                    />
+                    <span className="pt-0.5">
+                      TypeScript
+                    </span>
+                  </span>
+                  
+                  <span
+                    className="inline-flex rounded-full border py-2 px-4 gap-2.5 items-center justify-center leading-none text-sm text-[#B0F45E] border-[#B0F45E]"
+                  >
+                    <Image
+                      aria-hidden
+                      src="/techs/node.svg"
+                      alt="Node.js icon"
+                      width={16}
+                      height={16}
+                    />
+                    <span className="pt-0.5">
+                      Node.js
+                    </span>
+                  </span>
+                </div>
               </div>
             </div>
           </article>
