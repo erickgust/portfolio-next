f36764e64c457ae7616086557df58a89da0fc218
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2025-02-11 20:33:12 -0300

    feat: add scrolltrigger to horizontal lines

--- src/components/HorizontalLine.tsx
+++ src/components/HorizontalLine.tsx
@@ -1,14 +1,60 @@
+'use client'
+
+import { useGSAP } from '@gsap/react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { useRef } from 'react'
+
+gsap.registerPlugin(ScrollTrigger)
+
+export function useHorizontalLineScrollAnimation(start: 'left' | 'right') {
+  const lineRef = useRef<HTMLSpanElement>(null)
+
+  useGSAP(
+    () => {
+      ScrollTrigger.create({
+        trigger: lineRef.current,
+        start: 'top 90%',
+        end: 'bottom top',
+        invalidateOnRefresh: true,
+        once: true,
+        onEnter: () => {
+          gsap.fromTo(
+            lineRef.current,
+            {
+              scaleX: 0,
+              opacity: 0.3,
+              transformOrigin: start,
+            },
+            {
+              scaleX: 1,
+              opacity: 0.05,
+              duration: 1,
+            },
+          )
+        },
+      })
+    },
+    { scope: lineRef },
+  )
+
+  return lineRef
+}
+
 type HorizontalLineProps = {
   side: 'top' | 'bottom'
   start: 'left' | 'right'
 }
 
 export function HorizontalLine({ side, start }: HorizontalLineProps) {
+  const lineRef = useHorizontalLineScrollAnimation(start)
+
   return (
     <span
+      ref={lineRef}
       data-side={side}
       data-start={start}
-      className='animate-line-width absolute -z-10 block h-px w-screen bg-white data-[side="bottom"]:bottom-0 data-[side="top"]:top-0 data-[start="left"]:right-1/2 data-[start="right"]:left-1/2 data-[start="left"]:translate-x-1/2 data-[start="right"]:-translate-x-1/2'
+      className='absolute -z-10 block h-px w-screen bg-white opacity-0 data-[side="bottom"]:bottom-0 data-[side="top"]:top-0 data-[start="left"]:right-1/2 data-[start="right"]:left-1/2 data-[start="left"]:translate-x-1/2 data-[start="right"]:-translate-x-1/2'
     />
   )
 }
